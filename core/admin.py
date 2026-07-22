9aad5e4f66828b49d02b59df568b83a38769bcae
deji12 <adesolaayodeji53@gmail.com>
2026-07-22 09:14:02 +0100

    setup models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tracker, Subreddit, PostLead
 
-# Register your models here.
+class TrackerAdmin(admin.ModelAdmin):
+    list_display = ['last_fetched_subreddit', 'total_fetched_posts']
+
+admin.site.register(Tracker, TrackerAdmin)
+
+class SubredditAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Subreddit, SubredditAdmin)
+
+class PostLeadAdmin(admin.ModelAdmin):
+    list_display = ['subreddit', 'post_id', 'author_username', 'service_category', 'is_good_lead', 'recommended_action']
+    search_fields = ['post_id', 'subreddit__name', 'author_username']
+    list_filter = ['subreddit', 'service_category', 'is_good_lead']
+
+admin.site.register(PostLead, PostLeadAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Tracker(models.Model):
+    last_fetched_subreddit = models.CharField(max_length=30)
+    total_fetched_posts = models.IntegerField(default=0)
+
 # Create your models here.
+class Subreddit(models.Model):
+    name = models.CharField(max_length=50)
+    
+class PostLead(models.Model):
+    subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
+    post_id = models.CharField(max_length=15)
+    author_username = models.CharField(max_length=50)
+    content = models.TextField()
+    url = models.URLField()
+    service_category = models.CharField(max_length=20)
+    score = models.IntegerField()
+    estimated_value = models.FloatField(default=0)
+    is_good_lead = models.BooleanField(default=False)
+    buying_intent = models.CharField(max_length=20)
+    urgency = models.CharField(max_length=20)
+    competition = models.CharField(max_length=20)
+    recommended_action = models.CharField(max_length=20)
+    matched_keywords = models.JSONField()
+    ai_review = models.TextField()
+    suggested_replies = models.JSONField()
+
+    def __str__(self):
+        return f"{self.subreddit.name} -> {self.post_id}"
